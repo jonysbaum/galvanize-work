47bc4a8494f34d59db127c3ce877fdde7efab686
jonysbaum <jondnussbaum@gmail.com>
2023-01-09 10:12:31 -0600

    first one

---
+++ src/main/java/com/galvanize/Main.java
@@ -0,0 +1,9 @@
+package com.galvanize;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class Main {
+    public static void main(String[] args) {
+        String myString = "This is a String";
+    }
+}
